53b2cb1466ba98ae362e49a377224c80dc853732
iamrk-js <88290009+iamrk-js@users.noreply.github.com>
2023-05-26 12:10:15 +0530

    work on adv and basic courses impretive way

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,19 +13,24 @@ import { CoursesService } from '../services/courses.service';
 export class HomeComponent implements OnInit {
 
     courses: Array<Course> = [];
-    
+    beginnersCourses: Array<Course> = []
+    advanceCourses: Array<Course> = []
     constructor(private _coursesService: CoursesService) {
 
     }
 
     ngOnInit(): void {
-        this._coursesService.getAllCourses()
+        let courses$ = this._coursesService.getAllCourses()
             .pipe(
                 map(res => res['payload'])
-            )
+            );
+
+        courses$
             .subscribe(
                 res => {
                     this.courses = res;
+                    this.beginnersCourses = res.filter(course => course.category === "BEGINNER");
+                    this.advanceCourses = res.filter(course => course.category === "ADVANCED")
                 }
             )
     }
